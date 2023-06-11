68cf482c67cb6ef23f11e548d919cfac18e1221e
vdiezace <victoriadiezacereda@gmail.com>
2023-06-11 16:44:03 +0100

    cambiando el getAll de las categorias

--- apiTeacherApp/routes/categories.js
+++ apiTeacherApp/routes/categories.js
@@ -1,11 +1,11 @@
-const { getAll } = require("../models/student.model");
+const { getAllCategories } = require("../models/category.model");
 
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   //res.json("pasa por aqui");
   try {
-    const [categories] = await getAll();
+    const [categories] = await getAllCategories();
     res.json(categories);
   } catch (error) {
     res.status(500).json({ fatal: error.message });
